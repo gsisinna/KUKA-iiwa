a30acac4cf4cc540c33c8ba4cf20b5d7911d0291
unknown <unknown@example.com>
2020-12-03 18:56:02 +0100

    Nodes, msg, inv_kin fix
    Clik non converge ma dimensioni ora sono corrette. Fixati anche i nodi di esempio dai tutorial di ros e catkin compila correttamente con messaggio custom da sistemare per la posa dell'end-effector.

--- ros_packages/kuka_iiwa_14_prismatic_gripper/scripts/talker.py
+++ ros_packages/kuka_iiwa_14_prismatic_gripper/scripts/talker.py
@@ -1,52 +1,31 @@
 #!/usr/bin/env python
-# Software License Agreement (BSD License)
-#
-# Copyright (c) 2008, Willow Garage, Inc.
-# All rights reserved.
-#
-# Redistribution and use in source and binary forms, with or without
-# modification, are permitted provided that the following conditions
-# are met:
-#
-#  * Redistributions of source code must retain the above copyright
-#    notice, this list of conditions and the following disclaimer.
-#  * Redistributions in binary form must reproduce the above
-#    copyright notice, this list of conditions and the following
-#    disclaimer in the documentation and/or other materials provided
-#    with the distribution.
-#  * Neither the name of Willow Garage, Inc. nor the names of its
-#    contributors may be used to endorse or promote products derived
-#    from this software without specific prior written permission.
-#
-# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
-# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
-# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
-# FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
-# COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
-# INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
-# BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
-# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
-# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
-# LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
-# ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
-# POSSIBILITY OF SUCH DAMAGE.
-#
-# Revision $Id$
-
-## Simple talker demo that published std_msgs/Strings messages
-## to the 'chatter' topic
-
+import numpy as np
 import rospy
-from std_msgs.msg import String
+from std_msgs.msg import Float64MultiArray, MultiArrayDimension
+from numpy.random import random
 
 def talker():
-    pub = rospy.Publisher('chatter', String, queue_size=10)
+    pub = rospy.Publisher('ee_pose', Float64MultiArray, queue_size=10)
     rospy.init_node('talker', anonymous=True)
-    rate = rospy.Rate(10) # 10hz
+    rate = rospy.Rate(100) # 100hz
     while not rospy.is_shutdown():
-        hello_str = "hello world %s" % rospy.get_time()
-        rospy.loginfo(hello_str)
-        pub.publish(hello_str)
+        pose = Float64MultiArray()
+        pose.layout.dim.append(MultiArrayDimension(label='pose',size=6,stride=1))
+        pose.data = list(random(size=7)-0.5)
+        rospy.loginfo(pose)
+        pub.publish(pose)
+        rate.sleep()
+        vel = Float64MultiArray()
+        vel.layout.dim.append(MultiArrayDimension(label='vel',size=6,stride=1))
+        vel.data = list(random(size=7)-0.5)
+        rospy.loginfo(vel)
+        pub.publish(vel)
+        rate.sleep()
+        time = Float64MultiArray()
+        time.layout.dim.append(MultiArrayDimension(label='time',size=1,stride=1))
+        time.data = list(random(size=1) - 0.5)
+        rospy.loginfo(time)
+        pub.publish(time)
         rate.sleep()
 
 if __name__ == '__main__':
