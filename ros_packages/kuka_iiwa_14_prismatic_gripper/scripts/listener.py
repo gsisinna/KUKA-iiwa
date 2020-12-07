c0c722628dd035b93a0ab919f05ca1f566a58320
unknown <unknown@example.com>
2020-12-07 19:17:20 +0100

    Fixed callback functions, while, msgs. Still problems.

--- ros_packages/kuka_iiwa_14_prismatic_gripper/scripts/listener.py
+++ ros_packages/kuka_iiwa_14_prismatic_gripper/scripts/listener.py
@@ -4,6 +4,9 @@ import rospy
 from kuka_iiwa_14_prismatic_gripper.msg import end_effector
 from sensor_msgs.msg import JointState
 from std_msgs.msg import Float64MultiArray, MultiArrayDimension
+from trajectory_msgs.msg import JointTrajectory, JointTrajectoryPoint
+from std_msgs.msg import Header
+from std_msgs.msg import Duration
 
 import rbdl
 model = rbdl.loadModel("/home/gabriele/Documents/NRP/GazeboRosPackages/src/kuka_iiwa_14_prismatic_gripper/scripts/iiwa.urdf")
@@ -13,103 +16,98 @@ from numpy.linalg import inv
 from numpy.linalg import norm
 from numpy import matmul
 from numpy.random import random
-
 #import quaternion
 
-def callback(data):
-    rospy.loginfo('Joints Positions: %s', data.position[2:])
-    rospy.loginfo('Joints Velocity: %s', data.velocity[2:])
-    rospy.loginfo('Time: %s', rospy.get_time() )
+q = np.zeros(7)
+q_old = np.zeros(7)
+
+pose_des = np.zeros(6)
+command = np.zeros(7)
+
+def jointStateCallback(data):
+    global pose_des
+    global command
+    global q
+    global q_old
+
+    #rospy.loginfo('Joints Positions: %s', data.position)
+    #rospy.loginfo('Joints Velocity: %s', data.velocity)
     
-
-    it = 0
+    J = np.zeros((6,7))
     h = 0.01
-    th = 1e-5
-    eps = 1e-3
-    K_err = (1/h)*np.diag(np.full(6,1))
-
+    it = 0
+    K = (1/h) * np.diag(np.full(6,1))
     EE_LOCAL_POS = np.array((0.0,0.0,0.045))
-
-    #I valori dei giunti mi verranno forniti dal topic /iiwa/joint_states, per ora uso np.zeros()
-
-    # [gripper_left_joint, gripper_right_joint, iiwa_joint_1, iiwa_joint_2, iiwa_joint_3,
-    #   iiwa_joint_4, iiwa_joint_5, iiwa_joint_6, iiwa_joint_7]
+    k=0.001
+    
+    #q = [gripper_left_joint, gripper_right_joint, iiwa_joint_1, iiwa_joint_2, iiwa_joint_3, iiwa_joint_4, iiwa_joint_5, iiwa_joint_6, iiwa_joint_7]
 
     q = data.position[2:]
-    q = np.asarray(q) 
+    q = np.asarray(q)
     q_old = q
 
-    qd = data.velocity[2:]
-    qd = np.asarray(qd) 
-    qd_old = qd
-
-    qdd = np.zeros(7)  
-    qdd_old = qdd
-
-    #Questi invece mi vengono dati dall'altro nodo che genera la traiettoria per l'end effector, talker.py
-    x = rbdl.CalcBodyToBaseCoordinates(model, q, 7, EE_LOCAL_POS) # 3 componenti
-    rospy.loginfo('EndEffector Position: %s', x)
-    x_des = np.array([0, 0, 1.306])
-
-    xd = rbdl.CalcPointVelocity6D(model, q, qd, 7, EE_LOCAL_POS) # vettore di 6 componenti
-    rospy.loginfo('EndEffector Velocity: %s', xd)
-    xd_des = np.zeros(6)
-
-    R = rbdl.CalcBodyWorldOrientation(model, q, 7).T #matrice di rotazione  
-
-    #Orientamento RPY
+    qd = data.position[2:]
+    qd = np.asarray(qd)
+    
+    x = rbdl.CalcBodyToBaseCoordinates(model, q, 7, EE_LOCAL_POS)
+    xd = rbdl.CalcPointVelocity6D(model, q, qd, 7, EE_LOCAL_POS)
+    
+    R = rbdl.CalcBodyWorldOrientation(model, q, 7).T
     roll = np.arctan2(R[1,0], R[0,0])
     pitch = np.arctan2(-R[2,0], np.sqrt(R[2,1]**2 + R[2,2]**2))
     yaw = np.arctan2(R[2,1], R[2,2])
 
     pose = np.array([x[0], x[1], x[2], roll, pitch, yaw])
-    pose_des = np.array([x_des[0], x_des[1], x_des[2], roll, pitch, yaw])
-
-    rospy.loginfo('EndEffector Pose: %s', pose)
 
     err = pose_des - pose
 
-    J = np.zeros((6,7))
-    k=0.001 #damping factor?
+    #QUI INIZIAVA IL VECCHIO WHILE CHE MI MANDAVA IN LOOP IL PROGRAMMA
+    rbdl.CalcPointJacobian6D(model, q, 7, EE_LOCAL_POS, J)
+    rospy.loginfo('Jacobian: %s', J)
+    J_inv = matmul(J.transpose(), inv(matmul(J, J.transpose())+(k**2)*np.ones((6,6))))
+    q = q_old + h * matmul(J_inv, (xd+matmul(K,err)))         
+    command = q
 
-    while(1):
-        rbdl.CalcPointJacobian6D(model, q, 7, EE_LOCAL_POS, J)
-        rospy.loginfo('Jacobian: %s', J)
-        J_inv = matmul(J.transpose(), inv(matmul(J, J.transpose())+(k**2)*np.ones((6,6))))
-        q = q_old + h * matmul(J_inv, (xd+matmul(K_err,err))) 
+    q_old = q
+    it+=1
     
-        rbdl.UpdateKinematics(model, q, qd, qdd)
-        x = rbdl.CalcBodyToBaseCoordinates(model, q, 7, EE_LOCAL_POS)
-        xd = rbdl.CalcPointVelocity6D(model, q, qd, 7, EE_LOCAL_POS)
-    
-        R = rbdl.CalcBodyWorldOrientation(model, q, 7).T
 
-        roll = np.arctan2(R[1,0], R[0,0])
-        pitch = np.arctan2(-R[2,0], np.sqrt(R[2,1]**2 + R[2,2]**2))
-        yaw = np.arctan2(R[2,1], R[2,2])
+def endEffectorCallback(data):
+    global pose_des 
+    rospy.loginfo('End-Effector desired position: %s', data.position)
+    pose_des = np.array([data.position[0], data.position[1], data.position[2], data.position[3], data.position[4], data.position[5] ])
 
-        pose = np.array([x[0], x[1], x[2], roll, pitch, yaw])
-        pose_des = np.zeros(6)
-        rospy.loginfo('Pose: %s', pose)
-        
-        err = pose_des - pose
     
-        qd_old = qd
-        q_old = q
-        #time = rospy.get_rostime()
-        it+=1
     
-        print("Q: ", q)
-        print("Error: ", norm(err))
-        print("Iteration: ", it)
-        print("----------------------------------------------------------------------------------------")
-        
-        
-
 def listener():
     rospy.init_node('listener', anonymous=True)
-    rospy.Subscriber('/iiwa/joint_states', JointState, callback)
+    rospy.Subscriber('/ee_data', end_effector, endEffectorCallback)
+    rospy.Subscriber('/iiwa/joint_states', JointState, jointStateCallback)
+    pub = rospy.Publisher('/iiwa/iiwa_position_controller/command', JointTrajectory, queue_size=10)
+
+    joints_str = JointTrajectory()
+    joints_str.header = Header()
+    joints_str.header.stamp = rospy.Time.now()
+    joints_str.joint_names = ['gripper_left_joint', 'gripper_right_joint', 'iiwa_joint_1', 'iiwa_joint_2', 'iiwa_joint_3', 'iiwa_joint_4', 'iiwa_joint_5', 'iiwa_joint_6', 'iiwa_joint_7']
+    point=JointTrajectoryPoint()
+    point.positions = [0.0, 0.0, command[0], command[1], command[2], command[3], command[4], command[5], command[6]]
+    point.time_from_start = rospy.Duration(2)
+    joints_str.points.append(point)
+    pub.publish(joints_str)
+    rospy.loginfo("position updated")   
     rospy.spin()
+
+    # cmd = Float64MultiArray()
+    # cmd.layout.dim.append(MultiArrayDimension(label='torques',size=7,stride=1))
+    # cmd.data = list(command)
+    # pub.publish(cmd)
+    # rospy.loginfo("Comando inviato")
+    # rospy.spin()
+
+
+if __name__ == '__main__':
+    listener()
+
 
 #QUATERNIONI
 # teta = np.arccos((R[0,0]+R[1,1]+R[2,2]-1)/2)
@@ -119,6 +117,3 @@ def listener():
 # pose_des = np.quaternion(1, 1, 1, 1)
 # pose_des = pose_des / np.absolute(pose_des)
 # err = np.multiply(pose_des, np.conjugate(pose))
-
-if __name__ == '__main__':
-    listener()
--- ros_packages/kuka_iiwa_14_prismatic_gripper/scripts/talker.py
+++ ros_packages/kuka_iiwa_14_prismatic_gripper/scripts/talker.py
@@ -1,28 +1,32 @@
 #!/usr/bin/env python
 import numpy as np
+from numpy.random import random
+
 import rospy
 from kuka_iiwa_14_prismatic_gripper.msg import end_effector
-from numpy.random import random
 
 # Msg structure
 # Header header
 
-# float64[] positions
-# float64[] velocities
-
+# float64[] position
+# float64[] orientation
+# float64[] velocity
 # float64 time
 
 def talker():
     pub = rospy.Publisher('ee_data', end_effector, queue_size=10)
     rospy.init_node('talker', anonymous=True)
     rate = rospy.Rate(100) # 100hz
+    iteration = 0
     while not rospy.is_shutdown():
         data = end_effector()
-        data.positions = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
-        data.velocities = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
-        data.time = rospy.get_rostime()
+        data.position = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0] #x,y,z and RPY angles
+        #data.orientation = [[1,2,3], [4,5,6], [6,7,8]]
+        data.velocity = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
+        data.time = iteration
         rospy.loginfo(data)
         pub.publish(data)
+        iteration += 1
         rate.sleep()
         
 
