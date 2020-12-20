356e363e1da02e88fb8c4a572a340bb2439a111d
unknown <unknown@example.com>
2020-12-20 17:02:25 +0100

    Pick and place coordinates testing

--- ros_packages/kuka_iiwa_14_prismatic_gripper/scripts/listener.py
+++ ros_packages/kuka_iiwa_14_prismatic_gripper/scripts/listener.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 
+#Rospy and msg definitions
 import rospy
 from kuka_iiwa_14_prismatic_gripper.msg import end_effector
 from sensor_msgs.msg import JointState
@@ -9,10 +10,11 @@ from std_msgs.msg import Header
 from std_msgs.msg import Duration
 from std_msgs.msg import Float64
 
-
+#RBDL model
 import rbdl
 model = rbdl.loadModel("/home/gabriele/Documents/NRP/GazeboRosPackages/src/kuka_iiwa_14_prismatic_gripper/scripts/iiwa.urdf")
 
+#Numpy and mathematical libraries
 import numpy as np
 from numpy import pi
 from numpy.linalg import inv
@@ -20,39 +22,42 @@ from numpy.linalg import norm
 from numpy.random import random
 import quaternion
 
-pose_des = np.zeros(6)
-
 def jointStateCallback(data):
+    #Global variables with def. outside local scope
     global pose_des
-    
+    global q
+
     J = np.zeros((6,7))
-    h = 0.01
-    K = (2/h) * np.diag(np.full(6,1))
+    h = 0.01                                           #dt
+    K = (2/h) * np.diag(np.full(6,1))                  #Stability for eigenvalus < 2/h
     EE_LOCAL_POS = np.array((0.0,0.0,0.045))
-    k = 0.001
+    k = 0.001                                          #Damped least square method
 
     # q = [gripper_left_joint, gripper_right_joint, iiwa_joint_1, iiwa_joint_2, iiwa_joint_3, iiwa_joint_4, iiwa_joint_5, iiwa_joint_6, iiwa_joint_7]
     
     q = data.position[2:]
     q = np.asarray(q)
 
+
     qd = data.velocity[2:]
     qd = np.asarray(qd)
 
     #CALCOLO POSIZIONE DELL'END-EFFECTOR
-    x = rbdl.CalcBodyToBaseCoordinates(model, q, 7, EE_LOCAL_POS)
-    xd = rbdl.CalcPointVelocity6D(model, q, qd, 7, EE_LOCAL_POS)
+    x = rbdl.CalcBodyToBaseCoordinates(model, q, 7, EE_LOCAL_POS)  #x,y,z
+    xd = rbdl.CalcPointVelocity6D(model, q, qd, 7, EE_LOCAL_POS)   #vx,vy,vz,wx,wy,wx
 
     #rospy.loginfo("Posizione End-Effector: %s", x)
 
-    #ORIENTAMENTO
+    #Orientation
     R = rbdl.CalcBodyWorldOrientation(model, q, 7).T
-    
     quat = quaternion.from_rotation_matrix(R)
-    quat_des = np.quaternion(0, pose_des[0], pose_des[1], pose_des[2])
+    rospy.loginfo(quat)
+    quat_des = np.quaternion(0.5, pose_des[0], pose_des[1], pose_des[2])
     quat_des = quat_des
+
     err_quat = quat_des * quat.conjugate()
     err_quat = quaternion.as_float_array(err_quat)
+
     err_lin = np.array(([pose_des[3]-x[0], pose_des[4]-x[1], pose_des[5]-x[2]]))
 
     #ERRORE (angular-linear)
@@ -65,36 +70,31 @@ def jointStateCallback(data):
     # INVERSA + DAMPING (IMPROVED)
     l = 0.2 #lunghezza caratteristica dei link
     w = 1e-4 * l**3
-    I = 2 * np.diag(np.full(6,1))
+    I = 1 * np.diag(np.full(6,1))
+    
     J_inv = J.T.dot(inv( J.dot(J.transpose()) + (k**2) * I + w * np.diag(np.full(6,1))))
     
     # EULERO IN AVANTI PER Q: CLIK
     q = q + h * J_inv.dot( xd + K.dot(err) )
 
-    pub = rospy.Publisher('/iiwa/iiwa_position_controller/command', JointTrajectory, queue_size=1000)
-    
     #rqt_plot for the error
     pub_error = rospy.Publisher('/iiwa/iiwa_position_controller/error', Float64, queue_size=100)
-    
+    pub_error.publish(norm(err))
+
+    pub = rospy.Publisher('/iiwa/iiwa_position_controller/command', JointTrajectory, queue_size=100)
     #POSITION CONTROLLER
     joints_str = JointTrajectory()
     joints_str.header = Header()
     joints_str.header.stamp = rospy.Time.now()
     joints_str.joint_names = ['iiwa_joint_1', 'iiwa_joint_2', 'iiwa_joint_3', 'iiwa_joint_4', 'iiwa_joint_5', 'iiwa_joint_6', 'iiwa_joint_7']
-    
-    # point = JointTrajectoryPoint()
-    # point.positions = [q[0], q[1], q[2], q[3], q[4], q[5], q[6]]
-    # point.time_from_start = rospy.Duration(2)
-    # joints_str.points.append(point)
 
     point = JointTrajectoryPoint()
-    point.positions = [q[0], q[1], q[2], q[3], q[4], q[5], q[6]]
-    point.time_from_start = rospy.Duration(4)
+    #point.positions = [ q[6], q[5], q[4], q[3], q[2], q[1], q[0] ]
+    point.positions = [ q[0], q[1], q[2], q[3], q[4], q[5], q[6] ]
+    point.time_from_start = rospy.Duration(1)
     joints_str.points.append(point)
     
     pub.publish(joints_str)
-    pub_error.publish(norm(err))
-    
     rospy.loginfo("COMANDO INVIATO: %s", joints_str)
 
 
@@ -104,9 +104,14 @@ def endEffectorCallback(data):
 
     
 if __name__ == '__main__':
+    pose_des = np.zeros(6)
+    q = np.zeros(7)
+
     rospy.init_node('listener', anonymous=True)
     rospy.Subscriber('/ee_data', end_effector, endEffectorCallback)
     rospy.Subscriber('/iiwa/joint_states', JointState, jointStateCallback)
     rate = rospy.Rate(100)
     rate.sleep()
     rospy.spin()
+
+    
--- ros_packages/kuka_iiwa_14_prismatic_gripper/scripts/talker.py
+++ ros_packages/kuka_iiwa_14_prismatic_gripper/scripts/talker.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python
+
 import numpy as np
 from numpy.random import random
 from numpy import sin
 from numpy import cos
 from numpy import pi
+
 import rospy
 from kuka_iiwa_14_prismatic_gripper.msg import end_effector
 
@@ -16,25 +18,29 @@ from kuka_iiwa_14_prismatic_gripper.msg import end_effector
 # float64 time    
 
 def talker():
+    global time
     pub = rospy.Publisher('ee_data', end_effector, queue_size=100)
     rospy.init_node('talker', anonymous=True)
+    time = rospy.get_time()
     rate = rospy.Rate(100) # 100hz
     while not rospy.is_shutdown():
         data = end_effector()
-        data.position = [0.0, 0.0, 0.0, 0.0, 0.0, 1] # angular, linear
+        #data.position = [0.0, 0.0, 0.0, 0.0, 0.0, 1.305]
+        data.position = [-1.0, 0.0, 0.0, 0.0, 0.5, 0.8] # angular, linear
         data.velocity = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
         data.time = rospy.get_time()
         pub.publish(data)
+        rospy.loginfo(data)
         rate.sleep()
-        
+
 def trigTrajectory(k,s):
     time = rospy.get_time()
     trajectory = [0.0, 0.0, 0, s*sin(2*pi*k*time), s*cos(2*pi*k*time), 0.7]
     return trajectory
 
 
-
 if __name__ == '__main__':
+
     try:
         talker()
     except rospy.ROSInterruptException:
